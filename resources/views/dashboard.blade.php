40af9d59daee685807487812e0f75c62df1f02d7
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-10 23:14:35 -0500

    ✨ feat(dashboard): Add player management section with CRUD link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,31 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="flex flex-wrap">
+                        <div
+                            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+                            <div style="background-image: url('https://cloudfront-us-east-1.images.arcpublishing.com/infobae/HH3DFUJPYQDPXVPQO5KVMXOMZM.jpg')" class="w-full h-40 bg-cover rounded-t-lg"></div>
+                            <div class="p-5">
+                                <a href="#">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                        Jugadores
+                                    </h5>
+                                </a>
+                                <p class="mb-3 font-normal text-gray-700">
+                                    Aquí podrás ver todos los jugadores, crearlos, editrlos y eliminarlos.
+                                </p>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    Ir al CRUD
+                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
