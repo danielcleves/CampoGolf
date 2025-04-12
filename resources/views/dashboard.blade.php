edd2a467c8db556bb49efde862f17674a496eb1c
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-12 11:22:17 -0500

    ✨ feat(dashboard): Corregir errores tipográficos y agregar enlaces a CRUD de jugadores, campos y reservas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,9 +17,9 @@
                                 </h5>
                             </a>
                             <p class="mb-3 font-normal text-gray-700">
-                                Aquí podrás ver todos los jugadores, crearlos, editrlos y eliminarlos.
+                                Aquí podrás ver todos los jugadores, crearlos, editarlos y eliminarlos.
                             </p>
-                            <a href="#"
+                            <a href="{{ route('jugadores.index') }}"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                 Ir al CRUD
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
@@ -40,9 +40,32 @@
                                 </h5>
                             </a>
                             <p class="mb-3 font-normal text-gray-700">
-                                Aquí podrás ver todos los campos, crearlos, editrlos y eliminarlos.
+                                Aquí podrás ver todos los campos, crearlos, editarlos y eliminarlos.
                             </p>
-                            <a href="#"
+                            <a href="{{ route('campos.index') }}"
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                Ir al CRUD
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div
+                        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+                        <div style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/5/58/Golfcart.JPG')" class="w-full h-40 bg-cover bg-center rounded-t-lg"></div>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                    Reservas
+                                </h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700">
+                                Aquí podrás ver todos las reservas, crearlas, editarlas y eliminarlas.
+                            </p>
+                            <a href="{{ route('reservas.index') }}"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                 Ir al CRUD
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
